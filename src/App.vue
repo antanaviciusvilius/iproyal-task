f7804249a48a88a428baabe19766f78da7693946
ViliusAn <vilius.antanavicius123@gmail.com>
2023-11-03 11:07:43 +0200

    Init project. Delete unused files.

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="app">
+    <sidebar>
+      sidebar
+    </sidebar>
+    <div class="main-container">
+      <header>
+        header
+      </header>
+      <main class="main-content">
+        main content
+      </main>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.app {
+  display: grid;
+  grid-template-columns: var(--sidebar-width) 1fr;
+  flex: 1;
+}
+
+.main-container {
+  display: grid;
+  grid-template-rows: var(--header-height) 1fr;
+}
+</style>
