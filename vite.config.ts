66676f9e647a9231f70528331734367c35c30614
ViliusAn <vilius.antanavicius123@gmail.com>
2023-11-10 19:23:05 +0200

    Deployment configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,25 @@
 import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [
+      vue(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
+    base: '/',
+  };
+
+  if (command !== 'serve') {
+    config.base = '/iproyal-task/';
+  }
+
+  return config;
 });
